08d5d8c28f0a7fb964c76e88a3cec5ff2593c13a
Carl Thompson <carl.thompson1@gmail.com>
2012-07-21 16:33:46 -0500

    Now use case-insensitive email authentication

--- application/models/authenticate_model.php
+++ application/models/authenticate_model.php
@@ -7,14 +7,11 @@ class Authenticate_Model extends CI_Model {
     public function verify_user( $email, $password ) {
         $q = $this
            ->db
-           ->where( 'email', $email )
+           ->where( "LOWER( email ) = '" . strtolower( $email ) . "'" )
            ->where( 'password', sha1( $password ) )
            ->limit(1)
            ->get('users');
         if( $q->num_rows > 0 ) {
-            //echo '<pre>';
-            //print_r($q->row());
-            //echo '</pre>';
             return $q->row();
         }
         return false;
