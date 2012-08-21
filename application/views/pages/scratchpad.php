8370c29bf274fd693d1411ab79dc5231c59c5d45
JD Layman <jlayman2@gmail.com>
2012-08-20 21:40:54 -0500

    Added confirmation message before deleting a beer from the scratchpad.

--- application/views/pages/scratchpad.php
+++ application/views/pages/scratchpad.php
@@ -1,5 +1,9 @@
-
-<h2>My Scratchpad&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo anchor( base_url( "log/scratch/" ), "+ Add" ); ?></h2>
+<div class="page-header">
+    <h1>My Scratchpad</h1>
+</div>
+<div class="btn-group">
+    <?php echo anchor( base_url( 'log/scratch/' ), '<i class="icon-plus"></i> Add', array( 'class' => 'btn' ) ); ?>
+</div>
 <p>
 <?php
     if( count( $scratches ) > 0 ) {
@@ -9,28 +13,18 @@
         $this->table->set_template( $tmpl );
         $this->table->set_heading( '', '', '', 'Date', 'Brewery', 'Beer', 'Size', 'Rating', 'Notes' );
         foreach( $scratches as $scratch ) {
-            $edit_props = array(
-                'src' => 'img/pencil.png',
-                'alt' => 'Edit',
-            );
-            $edit    = anchor( base_url( 'log/scratch/' . $scratch[ 'scratchpad_id' ] ), img( $edit_props ), array( 'title' => 'Edit' ) );
-            $convert_props = array(
-                'src' => 'img/checkmark-green.png',
-                'alt' => 'Convert',
-            );
-            $convert  = anchor( base_url( 'log/scratch/' . $scratch[ 'scratchpad_id' ] . "/c/" ), img( $convert_props ), array( 'title' => 'Convert' ) );
-            $delete_props = array(
-                'src' => 'img/red-x.png',
-                'alt' => 'Delete',
-            );
-            $delete  = anchor( base_url( 'log/scratch/' . $scratch[ 'scratchpad_id' ] . "/x/" ), img( $delete_props ), array( 'title' => 'Delete' ) );
-            $date    = $scratch[ 'date' ];
-            $brewery = $scratch[ 'brewer' ];
-            $beer    = $scratch[ 'beer' ];
-            $sstyle  = $scratch[ 'substyle' ];
-            $size    = $scratch[ 'size' ];
-            $rating  = $scratch[ 'rating' ];
-            $notes   = $scratch[ 'notes' ];
+            $date      = $scratch[ 'date' ];
+            $brewery   = $scratch[ 'brewer' ];
+            $beer      = $scratch[ 'beer' ];
+            $sstyle    = $scratch[ 'substyle' ];
+            $size      = $scratch[ 'size' ];
+            $rating    = $scratch[ 'rating' ];
+            $notes     = $scratch[ 'notes' ];
+            $message   = 'Are you sure you want to delete ' . $beer . ' from your scratchpad?';
+            $deleteUrl = base_url( 'log/scratch/' . $scratch[ 'scratchpad_id' ] . '/x/' );
+            $edit      = anchor( base_url( 'log/scratch/' . $scratch[ 'scratchpad_id' ] ), '<i class="icon-pencil"></i>', array( 'title' => 'Edit' ) );
+            $convert   = anchor( base_url( 'log/scratch/' . $scratch[ 'scratchpad_id' ] . "/c/" ), '<i class="icon-ok"></i>', array( 'title' => 'Convert' ) );
+            $delete    = anchor( '#confirmationModal', '<i class="icon-remove"></i>', array( 'title' => 'Delete', 'onClick' => 'confirmMessage( \'Confirm Delete\', \'' . $message . '\', \'' . $deleteUrl .'\', \'Delete\');', 'data-toggle' => 'modal'));
             $this->table->add_row( $edit, $convert, $delete, $date, $brewery, $beer, $size, $rating, $notes );
         }
         echo $this->table->generate();
@@ -39,7 +33,18 @@
     }
 ?>
 </p>
+<div id="confirmationModal" class="modal hide fade">
+    <div class="modal-header"><h3>Test</h3></div>
+    <div class="modal-body">blah</div>
+    <div class="modal-footer">
+        <a class="btn" data-dismiss="modal">Close</a>
+        <a class="btn btn-primary" id="modal-link" href="#">Confirm</a>
+    </div>
+</div>
 <?php
     $source = base_url( "/js/" );
-    echo '<script type="text/javascript" src="' . $source . '/sorttable.js"></script>' ;
+    echo '<script type="text/javascript" src="' . $source . '/sorttable.js"></script>';
 ?>
+    <script type="text/javascript" src="<?php echo $source . '/jquery.js' ?>"></script>
+    <script type="text/javascript" src="<?php echo $source . '/bootstrap-modal.js' ?>"></script>
+    <script type="text/javascript" src="<?php echo $source . '/confirmation.js' ?>"></script></head>
---
+++ beer366.shraps.net/js/confirmation.js
@@ -0,0 +1,6 @@
+confirmMessage = function( title, body, link, linkText ) {
+		$('.modal-header h3').html(title);
+		$('.modal-body').html(body);
+		$('#modal-link').attr('href', link);
+		$('#modal-link').html(linkText);
+};
